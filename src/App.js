a4f193e1602b877ae0cef95487ed60450e1e5d34
Jackie Beltran <jacquelline500@gmail.com>
2020-11-11 15:13:40 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import ReactMapGL from 'react-map-gl';
 
 function App() {
+  const [viewport, setViewPort] = useState({
+    latitude: 34.0522,
+    longitude: 118.2437,
+    width: '100%vw',
+    height: '100vh',
+    zoom: 10,
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <ReactMapGL {...viewport}>
+
+     </ReactMapGL>
     </div>
   );
 }
