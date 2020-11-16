b19fc47d05561b6b496452d03596fba9b0c0e75a
Jackie Beltran <jacquelline500@gmail.com>
2020-11-16 11:14:04 -0800

    Add token

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import './App.css';
-import { useState } from 'react';
-import ReactMapGL from 'react-map-gl';
+import "./App.css";
+import { useState } from "react";
+import ReactMapGL from "react-map-gl";
 
 function App() {
   const [viewport, setViewPort] = useState({
     latitude: 34.0522,
-    longitude: 118.2437,
-    width: '100%vw',
-    height: '100vh',
+    longitude: -118.2437,
+    width: "50vw",
+    height: "50vh",
     zoom: 10,
   });
   return (
     <div className="App">
-     <ReactMapGL {...viewport}>
-
-     </ReactMapGL>
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        onViewStateChange={(viewport) => {
+          setViewPort(viewport);
+        }}
+      ></ReactMapGL>
     </div>
   );
 }
